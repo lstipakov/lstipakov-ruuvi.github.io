994084ca650cf23673bca46cf89f84f07e6aebef
lamminpaa <lamminpaakm@gmail.com>
2015-09-30 22:34:50 +0300

    deploy edited

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,5 @@ git commit -am "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push origin :master
 git subtree push --prefix build https://${GH_TOKEN}@github.com/ruuvi/ruuvi.github.io.git master
