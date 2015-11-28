73749537710572babcc02fef3d5f14338f529e46
lamminpaa <lamminpaakm@gmail.com>
2015-11-28 15:51:19 +0200

    removed build folder

--- build/assets/js/redirect.js
+++
@@ -1,6 +0,0 @@
-var lastDevice = localStorage.lastDevice;
-if(typeof(Storage) !== "undefined") {
-  if(lastDevice) {
-    window.location.replace('guide/getting-started/intro/' + lastDevice);
-  }
-}
